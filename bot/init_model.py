c412554bb7e4bd465da2c03728fca1d40de66e82
Fedor Balabanov <fedor.balabanov@list.ru>
2024-04-21 11:08:25 +0300

    remove onnx

--- bot/init_model.py
+++ bot/init_model.py
@@ -1,15 +1,21 @@
 import torch
+import os
 import pickle
 from numpy import array
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
-from transformers import pipeline, AutoTokenizer
+from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
 from optimum.onnxruntime import ORTModelForQuestionAnswering
 
 from .config import name_emb_model, name_qa_model, data_list, name_filter_emb, path_ml_filter_model 
 
 def init_qa_model(name_qa_model: str):
-    model = ORTModelForQuestionAnswering.from_pretrained(name_qa_model, from_transformers=True)
+    only_name = name_qa_model[name_qa_model.find("/") + 1:]
+    if name_qa_model in os.listdir("bot/models"):
+        with open("bot/models/" + only_name, "rb") as f:
+            return pickle.load(f)
+    # model = ORTModelForQuestionAnswering.from_pretrained(name_qa_model, from_transformers=True)
+    model = AutoModelForQuestionAnswering.from_pretrained(name_qa_model)
     tokenizer = AutoTokenizer.from_pretrained(name_qa_model)
     tokenizer.model_input_names = ['input_ids', 'attention_mask']
 
@@ -19,7 +25,8 @@ def init_qa_model(name_qa_model: str):
         tokenizer=tokenizer,
         device= torch.device("cuda" if torch.cuda.is_available() else "cpu")
         )
-    
+    with open("bot/models/" + only_name, "wb") as f:
+        pickle.dump(model_pipeline, f, protocol=-1)
     return model_pipeline
 
 def init_emb_model(name_emb_model: str, data_list: list):
